ad6625267c2203f73fa544a9d8ebf8d2b4bac42a
NhuongDT <inftech.ts91@gmail.com>
2023-06-28 17:15:19 +0700

    - SaoChep/Update HoaDon: fix error khuyenmai

--- banhang24/Scripts/GiaoDich/GiaoDichBan_Dat_Tra.js
+++ banhang24/Scripts/GiaoDich/GiaoDichBan_Dat_Tra.js
@@ -5528,7 +5528,7 @@
                         }
                         else {
                             // find hang tang kem of this HangHoa
-                            let hhTangKem = $.grep(cthd, function (x) {
+                            let hhTangKem = $.grep(arrCTsort, function (x) {
                                 return x.ID_TangKem === cthd.ID_DonViQuiDoi;
                             });
                             let txtKhuyenMai_Last = '';
