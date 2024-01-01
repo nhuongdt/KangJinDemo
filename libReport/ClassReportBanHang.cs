110caa78017fb0045e38db597dc06603d8dea02d
NhuongDT <inftech.ts91@gmail.com>
2024-01-01 15:28:27 +0700

    - ReportSale_ParamCommon: add {ThoiHanSuDung} --> check report BCGDV

--- libReport/ClassReportBanHang.cs
+++ libReport/ClassReportBanHang.cs
@@ -512,5 +512,6 @@ namespace libReport
         public string TheoDoi { get; set; }
         public string TrangThai { get; set; }
         public string IDChiNhanhs { get; set; }
+        public string ThoiHanSuDung { get; set; }
     }
 }
