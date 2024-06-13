0ddba5996886a79c146c657dd65cb7898bd8e86a
NhuongDT <inftech.ts91@gmail.com>
2024-06-13 15:59:33 +0700

    add migration AddTblKH_NVPhuTrach

---
+++ Model/KH_NVPhuTrach.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class KH_NVPhuTrach
+    {
+        [Key]
+        public Guid ID { get; set; }
+        public Guid ID_KhachHang { get; set; }
+        [ForeignKey("ID_KhachHang")]
+        public DM_DoiTuong DM_DoiTuong { get; set; }
+        public Guid ID_NhanVienPhuTrach { get; set; }
+        [ForeignKey("ID_NhanVienPhuTrach")]
+        public NS_NhanVien NS_NhanVien { get; set; }
+        public byte? VaiTro { get; set; }// 1. Tư vấn chính, 2. Tư vấn phụ, 3. Telesales, 4. NV chăm sóc đặc biệt
+    }
+}
---
+++ Model/Migrations/202406100632235_AddTblKH_NVPhuTrach.cs
@@ -0,0 +1,35 @@
+﻿namespace Model.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddTblKH_NVPhuTrach : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.KH_NVPhuTrach",
+                c => new
+                    {
+                        ID = c.Guid(nullable: false),
+                        ID_KhachHang = c.Guid(nullable: false),
+                        ID_NhanVienPhuTrach = c.Guid(nullable: false),
+                        VaiTro = c.Byte(),
+                    })
+                .PrimaryKey(t => t.ID)
+                .ForeignKey("dbo.DM_DoiTuong", t => t.ID_KhachHang, cascadeDelete: true)
+                .ForeignKey("dbo.NS_NhanVien", t => t.ID_NhanVienPhuTrach, cascadeDelete: true)
+                .Index(t => t.ID_KhachHang)
+                .Index(t => t.ID_NhanVienPhuTrach);
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.KH_NVPhuTrach", "ID_NhanVienPhuTrach", "dbo.NS_NhanVien");
+            DropForeignKey("dbo.KH_NVPhuTrach", "ID_KhachHang", "dbo.DM_DoiTuong");
+            DropIndex("dbo.KH_NVPhuTrach", new[] { "ID_NhanVienPhuTrach" });
+            DropIndex("dbo.KH_NVPhuTrach", new[] { "ID_KhachHang" });
+            DropTable("dbo.KH_NVPhuTrach");
+        }
+    }
+}
