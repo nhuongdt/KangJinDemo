ca5d5770af16d9ca243bca299c9ead68ec0c3fd0
NhuongDT <inftech.ts91@gmail.com>
2024-07-08 15:06:59 +0700

    add class ClassExcel_CellData

---
+++ libQuy_HoaDon/ClassExcel_CellData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace libQuy_HoaDon
+{
+    public class ClassExcel_CellData
+    {
+        public int RowIndex { get; set; }
+        public int ColumnIndex { get; set; }
+        public string CellValue { get; set; }// tiêu đề Data của file xuất ra (mặc định ghi ở dòng Cell[1,1]
+    }
+}
